2d211d8522fe9c316fe25ee8845d1417db33b56f
K4zuki <k.yamamoto.08136891@gmail.com>
2024-07-15 20:37:08 +0900

    update main.v
    - import lfsr12 and vga_syncgen modules
    - prepare VGA signals including each color output and oe

--- vga_random/ffpga/src/main.v
+++ vga_random/ffpga/src/main.v
@@ -1,32 +1,70 @@
 (* top *) module simple_counter(
   (* iopad_external_pin, clkbuf_inhibit *) input  i_clk,
+  (* iopad_external_pin, clkbuf_inhibit *) input  bitclock,
   (* iopad_external_pin *) output                 osc_en,
   (* iopad_external_pin *) input                  i_nreset,
-  (* iopad_external_pin *) output  [7:0]          o_lfsr,
-  (* iopad_external_pin *) output  [7:0]          lfsr_oe,
+  (* iopad_external_pin *) output  [3:0]          red,
+  (* iopad_external_pin *) output  [3:0]          red_oe,
+  (* iopad_external_pin *) output  [3:0]          green,
+  (* iopad_external_pin *) output  [3:0]          green_oe,
+  (* iopad_external_pin *) output  [3:0]          blue,
+  (* iopad_external_pin *) output  [3:0]          blue_oe,
+
+  (* iopad_external_pin *) output                 HSYNC,
+  (* iopad_external_pin *) output                 VSYNC,
+
+  (* iopad_external_pin *) output                 HSYNC_OE,
+  (* iopad_external_pin *) output                 VSYNC_OE,
+
   (* iopad_external_pin *) output reg             osc_out,
   (* iopad_external_pin *) output                 osc_out_en
 );
+/*
+|IO8  : R0  |IO12 : B0  |
+|IO9  : R1  |IO13 : B1  |
+|IO10 : R2  |IO14 : B2  |
+|IO11 : R3  |IO15 : B3  |
 
-  reg [7:0] lfsr;
-  assign lfsr_oe = 8'b11111111;
+|IO0 : G0   |IO4 : HSY  |
+|IO1 : G1   |IO5 : VSY  |
+|IO2 : G2   |IO6 : NC   |
+|IO3 : G3   |IO7 : NC   |
+*/
+
+  reg [11:0] lfsr;
+  wire display_on;
+  wire [9:0] hpos;
+  wire [9:0] vpos;
+  assign red_oe = 4'hF;
+  assign green_oe = 4'hF;
+  assign blue_oe = 4'hF;
   assign osc_en = 1'b1;
   assign osc_out_en = 1'b1;
+  assign HSYNC_OE = 1'b1;
+  assign VSYNC_OE = 1'b1;
 
   always @(posedge i_clk) begin
     osc_out = ~osc_out;
   end
 
-  always @(posedge i_clk) begin
-    //  http://www.neko.ne.jp/~freewing/fpga/lfsr_verilog/
-    if (i_nreset==1'b0)
-        lfsr <= 8'b11111111;
-    else begin
-        // 8 bit tap 8,6,5,4
-        lfsr <= {lfsr[0], lfsr[7], lfsr[0]^lfsr[6], lfsr[0]^lfsr[5], lfsr[0]^lfsr[4], lfsr[3:1]};
-    end
-  end
+  lfsr12 lfsr_gen(
+    .i_nreset (i_nreset),
+    .i_clk    (bitclock),
+    .lfsr     (lfsr    )
+  );
 
-  assign o_lfsr = lfsr;
+  vga_syncgen hvsync_gen(
+    .clk       (bitclock  ),
+    .reset     (i_nreset  ),
+    .hsync     (HSYNC     ),
+    .vsync     (VSYNC     ),
+    .display_on(display_on),
+    .hpos      (hpos      ),
+    .vpos      (vpos      )
+  );
+
+  assign red = {lfsr[11] && display_on, lfsr[10] && display_on, lfsr[9] && display_on, lfsr[8] && display_on};
+  assign green = {lfsr[7] && display_on, lfsr[6] && display_on, lfsr[5] && display_on, lfsr[4] && display_on};
+  assign blue = {lfsr[3] && display_on, lfsr[2] && display_on, lfsr[1] && display_on, lfsr[0] && display_on};
 
 endmodule
