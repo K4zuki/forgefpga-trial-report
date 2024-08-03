08957854d66a2035227b1d0c8c490ae102bae7a5
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2024-08-04 04:07:34 +0900

    gha challenge: checkout repo; add conversion script; run the script

---
+++ docx2pdf.ps1
@@ -0,0 +1,30 @@
+$word = NEW-OBJECT -COMOBJECT WORD.APPLICATION
+
+Write-Host "[docx2pdf.ps1]"
+$files = Get-ChildItem | Where-Object{ $_.Name -match "docx$" }
+Write-Host "[start]"
+foreach ($file in $files)
+{
+    try
+    {
+        $result = (Test-Path $file.FullName.Replace(".docx", ".pdf"))
+        if ($result)
+        {
+            Write-Host "$( $file.Name ) ... skip"
+        }
+        else
+        {
+            $doc = $word.Documents.OpenNoRepairDialog($file.FullName)
+            $doc.SaveAs([ref] $file.FullName.Replace(".docx", ".pdf"), [ref]17)
+            $doc.Close()
+            Write-Host "$($file.FullName.Replace(".docx", ".pdf") ) ... done"
+        }
+    }
+    catch
+    {
+        Write-Host "[ERROR]$( $file.Name ) ... failed"
+    }
+}
+Write-Host "[done]"
+#Pause
+$word.Quit()
