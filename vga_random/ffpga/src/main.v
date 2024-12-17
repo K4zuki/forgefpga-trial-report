61bd428e72ac48c082019598f207f9b06c607762
K4zuki <k.yamamoto.08136891@gmail.com>
2024-12-18 01:03:42 +0900

    update main.v - use PLL to get bit clock; add PLL config ports; RGB bits are clocked

--- vga_random/ffpga/src/main.v
+++ vga_random/ffpga/src/main.v
@@ -1,20 +1,29 @@
 (* top *) module simple_counter(
   (* iopad_external_pin, clkbuf_inhibit *) input  i_clk,
-  (* iopad_external_pin, clkbuf_inhibit *) input  bitclock,
+  (* iopad_external_pin, clkbuf_inhibit *) input  pll_clk,
   (* iopad_external_pin *) output                 osc_en,
   (* iopad_external_pin *) input                  i_nreset,
-  (* iopad_external_pin *) output  [3:0]          red,
-  (* iopad_external_pin *) output  [3:0]          red_oe,
-  (* iopad_external_pin *) output  [3:0]          green,
-  (* iopad_external_pin *) output  [3:0]          green_oe,
-  (* iopad_external_pin *) output  [3:0]          blue,
-  (* iopad_external_pin *) output  [3:0]          blue_oe,
+  (* iopad_external_pin *) output reg [3:0]       red,
+  (* iopad_external_pin *) output [3:0]           red_oe,
+  (* iopad_external_pin *) output reg [3:0]       green,
+  (* iopad_external_pin *) output [3:0]           green_oe,
+  (* iopad_external_pin *) output reg [3:0]       blue,
+  (* iopad_external_pin *) output [3:0]           blue_oe,
 
   (* iopad_external_pin *) output                 HSYNC,
   (* iopad_external_pin *) output                 VSYNC,
 
   (* iopad_external_pin *) output                 HSYNC_OE,
   (* iopad_external_pin *) output                 VSYNC_OE,
+
+  // PLL Settings
+  (* iopad_external_pin *) output                 pll_en,
+  (* iopad_external_pin *) output [5:0]           pll_refdiv,
+  (* iopad_external_pin *) output [11:0]          pll_fbdiv,
+  (* iopad_external_pin *) output [2:0]           pll_postdiv1,
+  (* iopad_external_pin *) output [2:0]           pll_postdiv2,
+  (* iopad_external_pin *) output                 pll_bypass,
+  (* iopad_external_pin *) output                 pll_clk_selection,
 
   (* iopad_external_pin *) output reg             osc_out,
   (* iopad_external_pin *) output                 osc_out_en
@@ -31,9 +40,18 @@
 |IO3 : G3   |IO7 : NC   |
 */
 
+  assign pll_en = 1'b1;
+  // config for 65MHz
+  assign pll_refdiv = 6'd1;
+  assign pll_fbdiv = 12'd26;
+  assign pll_postdiv1 = 3'd5;
+  assign pll_postdiv2 = 3'd4;
+  assign pll_bypass = 1'b0;// 1 lets PLL_IN = PLL_OUT
+  assign pll_clk_selection = 1'b0; // 1 uses GPIO2 for PLL input
+
   reg [11:0] lfsr;
   wire display_on;
-  wire [9:0] hpos;
+  wire [10:0] hpos;
   wire [9:0] vpos;
   assign red_oe = 4'hF;
   assign green_oe = 4'hF;
@@ -44,17 +62,17 @@
   assign VSYNC_OE = 1'b1;
 
   always @(posedge i_clk) begin
-    osc_out = ~osc_out;
+    osc_out <= ~osc_out;
   end
 
   lfsr12 lfsr_gen(
     .i_nreset (i_nreset),
-    .i_clk    (bitclock),
+    .i_clk    (pll_clk ),
     .lfsr     (lfsr    )
   );
 
   vga_syncgen hvsync_gen(
-    .clk       (bitclock  ),
+    .clk       (pll_clk   ),
     .reset     (i_nreset  ),
     .hsync     (HSYNC     ),
     .vsync     (VSYNC     ),
@@ -63,8 +81,11 @@
     .vpos      (vpos      )
   );
 
-  assign red = {lfsr[11] && display_on, lfsr[10] && display_on, lfsr[9] && display_on, lfsr[8] && display_on};
-  assign green = {lfsr[7] && display_on, lfsr[6] && display_on, lfsr[5] && display_on, lfsr[4] && display_on};
-  assign blue = {lfsr[3] && display_on, lfsr[2] && display_on, lfsr[1] && display_on, lfsr[0] && display_on};
+  always @(posedge pll_clk) begin
+    red <= {lfsr[11] && display_on, lfsr[10] && display_on, lfsr[9] && display_on, lfsr[8] && display_on};
+    green <= {lfsr[7] && display_on, lfsr[6] && display_on, lfsr[5] && display_on, lfsr[4] && display_on};
+    blue <= {lfsr[3] && display_on, lfsr[2] && display_on, lfsr[1] && display_on, lfsr[0] && display_on};
+  end
+
 
 endmodule
